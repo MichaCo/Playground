005d7e0ba16d393e8bc482f0f79ec2626d0d2699
MichaC <kletterer@web.de>
2016-12-08 21:32:13 +0100

    bare web service

---
+++ ForMahwish/src/AngularWeb/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AngularWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseWebRoot("wwwroot")
+                .UseStartup(typeof(Startup))
+                .Build();
+
+            host.Run();
+        }
+
+        class Startup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+            }
+
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            {
+                app.UseStaticFiles();
+            }
+        }
+    }
+}
